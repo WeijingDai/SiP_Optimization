9cd3646d44b4112e754357c2fe53f23a211904ec
WeijingDai <weijing.dai@gmail.com>
2020-05-08 20:15:38 +0800

    normalizaiton

--- bayesian_loop_2d_discrete_domain.py
+++ bayesian_loop_2d_discrete_domain.py
@@ -20,6 +20,7 @@ from botorch.models import SingleTaskGP
 from botorch.models import FixedNoiseGP
 import matplotlib.pyplot as plt
 from matplotlib import cm, ticker, colors
+from mpl_toolkits.mplot3d import axes3d
 print('Libraries imported')
 
 # %%
@@ -31,11 +32,16 @@ n_variables = 2
 #f_path = 'C:/Users/weijing/Documents/Nutstore/Abaqus_warpage_result/2_variables_result/'
 with open('./full_scan_2d.csv') as f:
     full_scan = np.loadtxt(f, delimiter=',').reshape(n_h_emc, n_cte_emc, n_variables+1)
-n_train_init = 1
+n_train_init = 2
+h_emc_max = full_scan[:, :, 0].max()
+h_emc_min = full_scan[:, :, 0].min()
+cte_emc_max = full_scan[:, :, 1].max()
+cte_emc_min = full_scan[:, :, 1].min()
 full_scan_norm = full_scan.copy()
-full_scan_norm[:, :, 0] = (full_scan_norm[:, :, 0] - 550)/20
-full_scan_norm[:, :, 1] = (full_scan_norm[:, :, 1] - 80)/2
+full_scan_norm[:, :, 0] = (full_scan_norm[:, :, 0] - h_emc_min)/(h_emc_max - h_emc_min)
+full_scan_norm[:, :, 1] = (full_scan_norm[:, :, 1] - cte_emc_min)/(cte_emc_max - cte_emc_min)
 
+# %%
 # Ramdon selection
 train_data = full_scan_norm[(random.randint(0, n_h_emc-1), random.randint(0, n_cte_emc-1))][None]
 while len(train_data)<n_train_init:
@@ -56,7 +62,9 @@ cbar1.set_label('Absolute warpage (um)')
 ax1.set_title('Absolute warpage mapping (um)')
 ax1.set_xlabel('EMC CTE (ppm)')
 ax1.set_ylabel('EMC thickness (um)')
-ax1.scatter(train_data[:, 1] * 0.2 + 8, train_data[:, 0] * 20 + 550, marker='.', c='green', s=100)
+ax1.scatter((train_data[:, 1] * (cte_emc_max - cte_emc_min) + cte_emc_min)/10, 
+            train_data[:, 0] * (h_emc_max - h_emc_min) + h_emc_min, 
+            marker='.', c='green', s=100)
 ax1.set_xlim(7.95, 12.05)
 ax1.set_ylim(545, 955)
 fig2, ax2 = plt.subplots()
@@ -68,7 +76,9 @@ cbar2.set_label('Squared warpage')
 ax2.set_title('Squared warpage mapping')
 ax2.set_xlabel('EMC CTE (ppm)')
 ax2.set_ylabel('EMC thickness (um)')
-ax2.scatter(train_data[:, 1] * 0.2 + 8, train_data[:, 0] * 20 + 550, marker='.', c='green', s=100)
+ax2.scatter((train_data[:, 1] *  (cte_emc_max - cte_emc_min) + cte_emc_min)/10, 
+            train_data[:, 0] * (h_emc_max - h_emc_min) + h_emc_min, 
+            marker='.', c='green', s=100)
 ax2.set_xlim(7.95, 12.05)
 ax2.set_ylim(545, 955)
 print('Initial data loaded')
@@ -82,7 +92,7 @@ print('Initial data loaded')
 # fr_path = 'Specific_selection/Right_bottom_two_diagonal/ucb_'+directory_list[j]+'/'
 # print(f_path+fr_path)
 
-acqf_para = 100
+acqf_para = 0.0
 print('Acquisition parameter = ', acqf_para)
 #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 device = torch.device('cpu')
@@ -96,7 +106,7 @@ best_observed_value = train_y.min().item()
 verbose = False
 # %%
 # Bayesian loop
-Trials = 100
+Trials = 20
 for trial in range(1, Trials+1):
 
     #print(f"\nTrial {trial:>2} of {Trials} ", end="\n")  
@@ -104,7 +114,9 @@ for trial in range(1, Trials+1):
 
     # fit the model
     # model = SingleTaskGP(train_x, -train_y).to(train_x)
-    model = FixedNoiseGP(train_x, -train_y, train_Yvar=torch.full_like(train_y, 1e-16)).to(train_x)
+    train_mu = train_y.mean()
+    train_sig = train_y.std()
+    model = FixedNoiseGP(train_x, -(train_y-train_mu)/train_sig, train_Yvar=torch.full_like(train_y, 1e-6)).to(train_x)
     mll = ExactMarginalLogLikelihood(model.likelihood, model)
     fit_gpytorch_model(mll)
 
@@ -125,24 +137,18 @@ for trial in range(1, Trials+1):
         j+=1 
     candidate_id = acqf_max[torch.randint(len(acqf_max), size=(1, ))]
     candidate = design_domain[candidate_id//n_h_emc, candidate_id%n_cte_emc]
-    # train_mean = model.posterior(design_domain).mean.detach().squeeze(-1)
-    # fig, ax = plt.subplots()
-    # cs = ax.contourf(cte_emc, h_emc, -train_mean,  
-    #                 locator=ticker.LogLocator(), cmap=cm.PuBu)
-    # cbar = fig.colorbar(cs)
-    # cbar.set_ticks([0.0000001, 0.000001, 0.00001, 0.0001, 0.001])
-    # cbar.set_label('Squared warpage')
-    # ax.set_title('Squared warpage mapping')
-    # ax.set_xlabel('EMC CTE (ppm)')
-    # ax.set_ylabel('EMC thickness (um)')
-    # ax.scatter(train_x[:, 1]/10, train_x[:, 0], marker='.', c='r', s=100)
-    # ax.set_xlim(7.95, 12.05)
-    # ax.set_ylim(545, 955)
-    # plt.show()
-    # print(acqf_max)
-    # print(candidate_id)
+    fig = plt.figure(figsize=(10, 7))
+    ax = fig.gca(projection='3d', azim= -60, elev=25)
+    mu = model.posterior(design_domain).mean.detach().squeeze(-1).numpy()
+    sigma = model.posterior(design_domain).variance.detach().squeeze(-1).numpy()
+    ax.plot_surface(cte_emc, h_emc, -mu * train_sig.numpy() + train_mu.numpy(), alpha = 0.5)
+    ax.plot_surface(cte_emc, h_emc, (-mu + 1.96 * sigma) * train_sig.numpy() + train_mu.numpy(), color='orange', alpha = 0.5)
+    ax.plot_surface(cte_emc, h_emc, (-mu - 1.96 * sigma) * train_sig.numpy() + train_mu.numpy(), color='orange', alpha = 0.5)
+    ax.plot(((candidate[:, 1] * (cte_emc_max - cte_emc_min) + cte_emc_min)/10, (candidate[:, 1] * (cte_emc_max - cte_emc_min) + cte_emc_min)/10), 
+            (candidate[:, 0] * (h_emc_max - h_emc_min) + h_emc_min, candidate[:, 0] * (h_emc_max - h_emc_min) + h_emc_min), 
+            ((-mu - 1.96 * sigma).min()* train_sig.numpy() + train_mu.numpy(), (-mu + 1.96 * sigma).max()* train_sig.numpy() + train_mu.numpy()), color='r')
+    plt.show()
     new_y = full_scan[candidate_id//n_h_emc, candidate_id%n_cte_emc, -1]
-    #print(new_y)
     train_new_y_origin = torch.as_tensor([[new_y]], dtype=dtype, device=device)
     train_new_y = train_new_y_origin**2
     # update training points
@@ -166,6 +172,8 @@ for trial in range(1, Trials+1):
             )
     else:
         print(".", end="")
+
+# %%
 optim_result = torch.cat([train_x, train_y_origin, train_y], 1)
 #np.savetxt(f_path+fr_path+'Optimization_loop.csv', optim_result.cpu().numpy(), delimiter=',')
 print('Bayesian loop completed')
@@ -179,9 +187,11 @@ cbar1.set_label('Absolute warpage (um)')
 ax.set_title('Absolute warpage mapping (um)')
 ax.set_xlabel('EMC CTE (ppm)')
 ax.set_ylabel('EMC thickness (um)')
-ax.scatter(train_data[:, 1] * 0.2 + 8, train_data[:, 0] * 20 + 550, marker='.', c='green')
-x1_plot = train_x.numpy()[len(train_data):, 1] * 0.2 + 8
-x2_plot = train_x.numpy()[len(train_data):, 0] * 20 + 550
+ax.scatter((train_data[:, 1] * (full_scan[:, :, 1].max() - full_scan[:, :, 1].min()) + full_scan[:, :, 1].min())/10, 
+            train_data[:, 0] * (full_scan[:, :, 0].max() - full_scan[:, :, 0].min()) + full_scan[:, :, 0].min(), 
+            marker='.', c='green', s=100)
+x1_plot = (train_x.numpy()[len(train_data):, 1] * (full_scan[:, :, 1].max() - full_scan[:, :, 1].min()) + full_scan[:, :, 1].min())/10
+x2_plot = train_x.numpy()[len(train_data):, 0] * (full_scan[:, :, 0].max() - full_scan[:, :, 0].min()) + full_scan[:, :, 0].min()
 c_plot = np.arange(1, len(x1_plot)+1)
 s1 = ax.scatter(x1_plot, x2_plot, marker ='*', c=c_plot, cmap=cm.plasma, alpha=0.5, s=c_plot)
 cbar2 = fig.colorbar(s1, orientation="horizontal")
@@ -190,7 +200,7 @@ cbar2.set_label('Iteration')
 ax.set_xlim(7.95, 12.05)
 ax.set_ylim(545, 955)
 plt.show()
-#fig.savefig(f_path+fr_path+'Optimization_loop.jpg', dpi=600)
+# fig.savefig(f_path+fr_path+'Optimization_loop.jpg', dpi=600)
 
 fig1, ax1 = plt.subplots()
 y_plot = abs(train_y_origin.numpy())*1000
@@ -199,7 +209,7 @@ ax1.set_yscale('log')
 #ax1.set_ylim(10**-(3.7), 10**-(1.6))
 ax1.set_xlabel('Loop iteration', fontsize='large')
 ax1.set_ylabel('Absolute warpage (um)', fontsize='large')
-#fig1.savefig(f_path+fr_path+'Warpage_reduction.jpg', dpi=600)
+# fig1.savefig(f_path+fr_path+'Warpage_reduction.jpg', dpi=600)
 
 ## %%
 # def heatmap(data, row_labels, col_labels, row_name, col_name, fig_name, ax=None,
@@ -271,6 +281,17 @@ ax1.set_ylabel('Absolute warpage (um)', fontsize='large')
 # # Create and change directory accordingly
 # fig.savefig(f_path+'/ucb_1/1/h_emc_vs_cte_emc_mapping_pixel.jpg', dpi=500, bbox_inches = 'tight')
 ## %%
+
+
+# %%
+    fig = plt.figure(figsize=(10, 7))
+    ax = fig.gca(projection='3d', azim= -60, elev=25)
+    mu = model.posterior(design_domain).mean.detach().squeeze(-1).numpy()
+    sigma = model.posterior(design_domain).variance.detach().squeeze(-1).numpy()
+    ax.plot_surface(cte_emc, h_emc, full_scan[:, :, -1]**2)
+    #ax.plot_surface(cte_emc, h_emc, -mu + 1.96 * sigma, color='orange', alpha = 0.8)
+    #ax.plot_surface(cte_emc, h_emc, -mu - 1.96 * sigma, color='orange')
+    #ax.plot((12., 12.), (750., 750.), (0., 3.5), color='r')
 
 
 # %%
